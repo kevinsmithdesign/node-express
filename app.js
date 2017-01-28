4543a9b24766479ff814745977165df75f9d3590
Kevin Smith <mrkevinsmithdesign@yahoo.com>
2017-01-27 18:54:39 -0600

    Experimented with the EJS Template Engine. Will try handlebars and Jade in the future.

--- app.js
+++ app.js
@@ -13,15 +13,56 @@ var logger = function(req,res, next) {
 app.use(logger);
 */
 
+//View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 // Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 //Set Static Path, Then I created a public folder for static resources such as CSS files or HTML.
-app.use(express.static(path.join(__dirname, 'public')));
+/* app.use(express.static(path.join(__dirname, 'public'))); */
+
+/* 
+var people = [{
+	name:'Jeff',
+	age: 30
+},{
+	name:'Lisa',
+	age: 22
+},{
+	name:'Bill',
+	age: 40
+}] 
+
+*/
+
+var users = [{
+
+	id: 1,
+	first_name: 'John',
+	last_name: 'Doe',
+	email: 'johndoe@gmail.com',
+},{
+
+	id: 2,
+	first_name: 'Bob',
+	last_name: 'Smith',
+	email: 'bobsmith@gmail.com',
+},{
+	id: 3,
+	first_name: 'Jill',
+	last_name: 'Jackson',
+	email: 'jjackson#gmail.com',
+}]
 
 app.get('/', function(req,res){
-	res.send('Hello World');
+	/* var title = 'Customers';*/
+	res.render('index', {
+		title: 'Customers',
+		users: users
+	});
 });
 
 app.listen(3000, function(){
